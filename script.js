279ad12cd0bf20f31397fbd85029d218193bb63c
calubnathaniel40 <calubnathaniel40@gmail.com>
2026-08-29 23:14:42 +0800

    Add love function to display a message

---
+++ script.js
@@ -0,0 +1,8 @@
+function love() {
+
+    const message = document.getElementById("message");
+
+    message.innerHTML =
+        "I love you, baby. ❤️ No matter what happens, I'm always rooting for us. 🥹";
+
+}
