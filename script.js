a27c6b9834400f06ded754928351580e84b5658c
calubnathaniel40 <calubnathaniel40@gmail.com>
2026-08-29 23:28:10 +0800

    Change message text in script.js

--- script.js
+++ script.js
@@ -3,6 +3,6 @@ function love() {
     const message = document.getElementById("message");
 
     message.innerHTML =
-        "I love you, baby. ❤️ No matter what happens, I'm always rooting for us. 🥹";
+        "BIMBANG NA TAYUU. 🥹";
 
 }
